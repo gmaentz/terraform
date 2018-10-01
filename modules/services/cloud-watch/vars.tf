712cb4d0d448170984d0d48fdb29f1d8f6606063
Gabe Maentz <gabe@maentz.net>
2018-10-01 09:06:36 -0400

    Update vars.tf for cloud-watch module

---
+++ modules/services/cloud-watch/vars.tf
@@ -0,0 +1,7 @@
+variable "autoscaling_group" {
+   description = "The auto scaling group that should be monitored."
+ }
+
+variable "sms_number" {
+   description = "The number in which to send SMS text messages for Alerts, in format +14126552983"
+ }
